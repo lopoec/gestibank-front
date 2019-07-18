d4a540451f47cd923f057b68f29f4295774a7a6f
loic <loic.bugnicourt@hotmail.com>
2019-07-18 12:12:39 +0200

    inscription component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InscriptionComponent } from './inscription/inscription.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InscriptionComponent
   ],
   imports: [
     BrowserModule,
