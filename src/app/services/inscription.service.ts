30ef8ca1bca30028beb6e3a1b5027d1df70fc574
noe38 <noemie.graignic@free.fr>
2019-07-19 10:00:15 +0200

    modification de la création de client potentiel mais pas encore fonctionnel

--- src/app/services/inscription.service.ts
+++ src/app/services/inscription.service.ts
@@ -5,9 +5,10 @@ export class ClientPotentiel{
   constructor(
     public nom:string,
     public prenom:string,
+    public email:string,
     public adresse:string,
     public telephone:string,
-    public revenuMensuel:string,
+    public revenuMens:string,
     public piecesJustif:string,
   ) {}
 }
